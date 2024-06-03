89e718ff9faa53e7fa62c00be4fa1c1ea33b5fdb
adi790uu <adi37068@gmail.com>
2024-06-03 12:00:57 +0530

    server push

--- server/go.mod
+++ server/go.mod
@@ -3,17 +3,21 @@ module github.com/Genesis-ISE-DSCE/HackMan-QR-Connect
 go 1.22.3
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
-	github.com/jackc/pgx/v5 v5.5.5 // indirect
-	github.com/jackc/puddle/v2 v2.2.1 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	go.mongodb.org/mongo-driver v1.15.0
+)
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
-	gorm.io/driver/postgres v1.5.7 // indirect
-	gorm.io/gorm v1.25.10 // indirect
+	// github.com/Genesis-ISE-DSCE/HackMan-QR-Connect/server/utils
 )
--- server/main.go
+++ server/main.go
@@ -1,58 +1,43 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
-	"github.com/skip2/go-qrcode"
-
-	"bufio"
 	"io"
 	"os"
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/skip2/go-qrcode"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
-}
+var client *mongo.Client
 
-func generateQR(code string, filename string) {
-    qrCode, err := qrcode.Encode(code, qrcode.Medium, 256)
-    if err!= nil {
+func GenerateQR(code string, filename string) {
+    qrCode, err := qrcode.Encode(fmt.Sprintf("http://localhost:5173/%v", code), qrcode.Medium, 256)
+    if err != nil {
         fmt.Printf("Error encoding QR code: %s\n", err)
         return
     }
 
-    err = os.WriteFile(filename, qrCode, 0644)
-    if err!= nil {
+    err = os.WriteFile(fmt.Sprintf("./qrs/%v", filename), qrCode, 0644)
+    if err != nil {
         fmt.Printf("Error writing QR code to file: %s\n", err)
         return
     }
 
     fmt.Printf("QR code generated and saved as %s\n", filename)
-}
-
-func getUserDetails(w http.ResponseWriter, r *http.Request) {
-	/*
-		1. DB calls to gather user information with team
-		2. Get Dinner, Breakfast and snacks status of the user from DB
-		3. Send all the details as in JSON format to the dashboard
-	*/ 
-}
-
-func postFoodUpdate(w http.ResponseWriter, r *http.Request) {
-	/* 
-		1. Get the body in request
-		2. Update the status for the food
-		3. Return confirmation
-	*/
 }
 
 func LoadEnv(filename string) error {
@@ -94,111 +79,352 @@ func LoadEnv(filename string) error {
     return nil
 }
 
-func auth(w http.ResponseWriter, r *http.Request) {
-	type TokenResponse struct {
-		Token string `json:"token"`
+func dbStart() (*mongo.Client, context.Context) {
+ 
+    var uri = os.Getenv("DB_URL")
+    
+    client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
 	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer client.Disconnect(ctx)
 
-	err := LoadEnv(".env")
+    err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+    return client, ctx
+}
+
+
+func init() {
+    err := LoadEnv(".env")
     if err != nil {
         fmt.Printf("Error loading .env file: %v\n", err)
         os.Exit(1)
     }
+}
 
-	if r.Method != "POST" {
-		http.Error(w,"Method is not supported", http.StatusNotFound)
-		return
-	}
-	bodybytes,err := io.ReadAll(r.Body)
-	if err!=nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
+type Participants struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"` // MongoDB document ID
+	TeamName  string             `bson:"teamName"`      // Required field
+	Email     string             `bson:"email"`         // Required field
+	PhoneNum  string             `bson:"phoneNum"`      // Required field
+	Name      string             `bson:"name"`          // Required field
+	Breakfast bool               `bson:"breakfast"`     // Default: false
+	Lunch     bool               `bson:"lunch"`         // Default: false
+	Dinner    bool               `bson:"dinner"`        // Default: false
+	Snack1    bool               `bson:"snack1"`        // Default: false
+	Snack2    bool               `bson:"snack2"`        // Default: false
+	CreatedAt time.Time          `bson:"createdAt,omitempty"` // Timestamps
+	UpdatedAt time.Time          `bson:"updatedAt,omitempty"` // Timestamps
+}
 
-	var creds struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
+func getUserDetails(w http.ResponseWriter, r *http.Request) {
+    // Extract ID from the URL path
+    type UserDetailsResponse struct {
+        Participant Participants `json:"participant"`
+    }
 
-	err = json.Unmarshal(bodybytes, &creds)
-	if err!= nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
+    pathParts := strings.Split(r.URL.Path, "/")
+    if len(pathParts) < 4 {
+        http.Error(w, "Invalid path", http.StatusBadRequest)
+        return
+    }
+    idStr := pathParts[3]
 
-	envUsername := os.Getenv("Username")
-	envPassword := os.Getenv("Password")
+    // Convert the string ID to a MongoDB ObjectID
+    objID, err := primitive.ObjectIDFromHex(idStr)
+    if err != nil {
+        http.Error(w, "Invalid ObjectID", http.StatusBadRequest)
+        return
+    }
 
-	if creds.Username != envUsername || creds.Password != envPassword {
-		http.Error(w, "Unauhorised access", http.StatusUnauthorized)
-		return;
-	}
+    // Create a timeout context for the MongoDB operation
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
 
-	
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-	claims["username"] = creds.Username
-	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+    // Assuming you have a MongoDB client instance named 'client'
+    database := client.Database("hackman-qr")
+    participantCollection := database.Collection("participants")
 
-	tokenString, err:= token.SignedString([]byte("yoursec"))
-	if err !=nil {
-		http.Error(w,"failed token creation", http.StatusInternalServerError)
-		return
-	}
-	tokenS := TokenResponse{
+    // Query the collection using the ObjectID
+    var participant Participants
+    err = participantCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&participant)
+    if err != nil {
+        if err == mongo.ErrNoDocuments {
+            http.Error(w, "No participant found", http.StatusNotFound)
+        } else {
+            log.Printf("Error finding participant: %v", err)
+            http.Error(w, "Error fetching participant", http.StatusInternalServerError)
+        }
+        return
+    }
+
+    // Prepare the response
+    participantS := UserDetailsResponse{
+        Participant: participant,
+    }
+
+    jsonResponse, err := json.Marshal(participantS)
+    if err != nil {
+        log.Printf("Error marshaling JSON: %v", err)
+        http.Error(w, "Error generating JSON", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonResponse)
+}
+
+func postFoodUpdate(w http.ResponseWriter, r *http.Request) {
+    // Extract ID from the URL path
+    pathParts := strings.Split(r.URL.Path, "/")
+    if len(pathParts) < 4 {
+        http.Error(w, "Invalid path", http.StatusBadRequest)
+        return
+    }
+    idStr := pathParts[3]
+
+    bodyBytes, err := io.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Bad Request", http.StatusBadRequest)
+        return
+    }
+    defer r.Body.Close()
+
+    var meal struct {
+        Meal string `json:"meal"`
+    }
+
+    err = json.Unmarshal(bodyBytes, &meal)
+    if err != nil {
+        http.Error(w, "Bad Request", http.StatusBadRequest)
+        return
+    }
+
+    objID, err := primitive.ObjectIDFromHex(idStr)
+    if err != nil {
+        http.Error(w, "Invalid ObjectID", http.StatusBadRequest)
+        return
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    database := client.Database("hackman-qr")
+    participantCollection := database.Collection("participants")
+
+    var participant Participants
+    err = participantCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&participant)
+    if err != nil {
+        if err == mongo.ErrNoDocuments {
+            http.Error(w, "No participant found", http.StatusNotFound)
+        } else {
+            log.Printf("Error finding participant: %v", err)
+            http.Error(w, "Error fetching participant", http.StatusInternalServerError)
+        }
+        return
+    }
+
+    var res bool
+    switch meal.Meal {
+    case "breakfast":
+        res = !participant.Breakfast
+        participant.Breakfast = res
+    case "lunch":
+        res = !participant.Lunch
+        participant.Lunch = res
+    case "dinner":
+        res = !participant.Dinner
+        participant.Dinner = res
+    case "snack1":
+        res = !participant.Snack1
+        participant.Snack1 = res
+    case "snack2":
+        res = !participant.Snack2
+        participant.Snack2 = res
+    default:
+        http.Error(w, "Invalid meal type", http.StatusBadRequest)
+        return
+    }
+
+    _, err = participantCollection.UpdateByID(ctx, objID, bson.M{"$set": bson.M{meal.Meal: res}})
+    if err != nil {
+        if err == mongo.ErrNoDocuments {
+            http.Error(w, "No participant found", http.StatusNotFound)
+        } else {
+            log.Printf("Error updating participant: %v", err)
+            http.Error(w, "Error updating participant", http.StatusInternalServerError)
+        }
+        return
+    }
+
+    jsonResponse, err := json.Marshal(participant)
+    if err != nil {
+        log.Printf("Error marshaling JSON: %v", err)
+        http.Error(w, "Error generating JSON", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonResponse)
+}
+
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+		if len(authHeader) != 2 {
+			fmt.Println("Malformed token")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Malformed Token"))
+		} else {
+			jwtToken := authHeader[1]
+			token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+				return []byte("SECRET"), nil
+			})
+			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+				ctx := context.WithValue(r.Context(), "props", claims)
+				// Access context values in handlers like this
+				// props, _ := r.Context().Value("props").(jwt.MapClaims)
+				next.ServeHTTP(w, r.WithContext(ctx))
+			} else {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Unauthorized"))
+			}
+		}
+	})
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+    type TokenResponse struct {
+        Token string `json:"token"`
+    }
+
+    if r.Method != "POST" {
+        http.Error(w, "Method is not supported", http.StatusNotFound)
+        return
+    }
+    bodyBytes, err := io.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Bad Request", http.StatusBadRequest)
+        return
+    }
+    defer r.Body.Close()
+
+    var creds struct {
+        Username string `json:"username"`
+        Password string `json:"password"`
+    }
+
+    err = json.Unmarshal(bodyBytes, &creds)
+    if err != nil {
+        http.Error(w, "Bad Request", http.StatusBadRequest)
+        return
+    }
+
+    envUsername := os.Getenv("Username")
+    envPassword := os.Getenv("Password")
+
+    if creds.Username != envUsername || creds.Password != envPassword {
+        http.Error(w, "Unauthorized access", http.StatusUnauthorized)
+        return
+    }
+
+    token := jwt.New(jwt.SigningMethodHS256)
+    claims := token.Claims.(jwt.MapClaims)
+    claims["username"] = creds.Username
+
+    tokenString, err := token.SignedString([]byte("SECRET"))
+    if err != nil {
+        http.Error(w, "Failed token creation", http.StatusInternalServerError)
+        return
+    }
+    tokenS := TokenResponse{
         Token: tokenString,
     }
 
-	jsonResponse, err := json.Marshal(tokenS)
+    jsonResponse, err := json.Marshal(tokenS)
     if err != nil {
         http.Error(w, "Error generating JSON", http.StatusInternalServerError)
         return
     }
 
-    
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
-
-    
     w.Write(jsonResponse)
 }
 
 func main() {
 
-	err := LoadEnv(".env")
+    err := LoadEnv(".env")
+
+    client, _ = dbStart()
+    
     if err != nil {
         fmt.Printf("Error loading .env file: %v\n", err)
         os.Exit(1)
     }
 
-	type Product struct {
-		gorm.Model
-		Code  string
-		Price uint
-	}
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
 
-	generateQR("http://your-dashboard-url/123", "adi.png")
+    // Assuming you have a MongoDB client instance named 'client'
+    // database := client.Database("hackman-qr")
+    // participantsCollection := database.Collection("participants")
 
-	http.HandleFunc("/", getHello)
-	http.HandleFunc("/auth", auth)
-	http.HandleFunc("/user/details/:id", getUserDetails)
-	http.HandleFunc("/user/update/:id", postFoodUpdate)
+    // Query the collection using the ObjectID
 
-	
+    // cur, err := participantsCollection.Find(ctx, bson.M{})
+    // if err != nil {
+    //     log.Printf("Error finding participants: %v", err)
+    //     return
+    // }
+    // defer cur.Close(ctx)
 
-	db, err := gorm.Open(postgres.Open(os.Getenv("DB_URL")), &gorm.Config{})
+    // var participants []Participants
+    // if err = cur.All(ctx, &participants); err != nil {
+    //     log.Printf("Error decoding participants: %v", err)
+    //     return
+    // }
+
+    // fmt.Println(participants)
+
+    // for i := 0; i < len(participants); i++ {
+    //     idText, err := participants[i].ID.MarshalText()
+    //     if err != nil {
+    //         log.Printf("Error marshaling ID: %v", err)
+    //         continue
+    //     }
+    //     GenerateQR(string(idText), fmt.Sprintf("%v.png", participants[i].Name))
+    // }
+
+    databases, err := client.ListDatabaseNames(ctx, bson.M{})
 	if err != nil {
-		log.Fatalf("Failed to connect to db %v", err)
+		log.Fatal(err)
 	}
-	fmt.Println(db)
 
-	db.AutoMigrate(&Product{})
-	db.Create(&Product{Code: "D42", Price: 100})
+    fmt.Println(databases)
+    http.HandleFunc("/auth", auth)
+    http.Handle("/user/details/", middleware(http.HandlerFunc(getUserDetails)))
+    http.Handle("/user/update/", middleware(http.HandlerFunc(postFoodUpdate)))
 
-    if err := http.ListenAndServe(":7500", nil); err!= nil {
+    if err := http.ListenAndServe(":7500", nil); err != nil {
         log.Fatalf("Failed to start server: %v", err)
-    }else {
-		fmt.Println("Server is running on localhost:7500...")
-	}
+    } else {
+        fmt.Println("Server is running on localhost:7500...")
+    }
 }
