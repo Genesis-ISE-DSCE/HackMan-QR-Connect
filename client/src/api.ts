14f20b712cd632aba4bd66e78bb7bef372e1d94e
Aditya <code.adi8103@gmail.com>
2024-06-07 01:41:28 +0530

    prod url

--- client/src/api.ts
+++ client/src/api.ts
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 const connector = axios.create({
-  baseURL: "http://localhost:7500",
+  baseURL: "https://hackman-qr-connect-production.up.railway.app",
 });
 
 export default connector;
